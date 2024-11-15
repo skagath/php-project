a0be5f31c6ee37a81d76d2595462ed06f7d378b1
skagath <155049450+skagath@users.noreply.github.com>
2024-11-15 16:30:34 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590183786246.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 438465160558.dkr.ecr.us-east-1.amazonaws.com
 docker pull 590183786246.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
