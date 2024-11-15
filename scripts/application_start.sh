4e35a184c282d5e508e92d9a01961f3707d7ccda
skagath <155049450+skagath@users.noreply.github.com>
2024-11-15 13:27:04 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop my-container || true
 docker rm my-container || true
-docker run -d --name my-container -p 80:80 590183786246.dkr.ecr.us-east-1.amazonaws.com/php-app:latest
+docker run -d --name my-container -p 80:80 438465160558.dkr.ecr.us-east-1.amazonaws.com/my-ecr-repo
